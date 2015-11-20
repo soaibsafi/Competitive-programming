8e8cb10e6f46d8fe22b7a7095547539e6a493800
soaibsafi <soaib.safi@gmail.com>
2015-11-20 12:50:41 +0600

    Create 12015.cpp

---
+++ 12015.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<cstdio>
+#include<cstring>
+using namespace std;
+int main()
+{
+    char a1[100],a2[100],a3[100],a4[100],a5[100],a6[100],a7[100],a8[100],a9[100],a0[100];
+    int num[10];
+
+    scanf("%s",a0);
+    cin>>num[0];
+    scanf("%s",a1);
+    cin>>num[1];
+    scanf("%s",a2);
+    cin>>num[2];
+    scanf("%s",a3);
+    cin>>num[3];
+    scanf("%s",a4);
+    cin>>num[4];
+    scanf("%s",a5);
+    cin>>num[5];
+    scanf("%s",a6);
+    cin>>num[6];
+    scanf("%s",a7);
+    cin>>num[7];
+    scanf("%s",a8);
+    cin>>num[8];
+    scanf("%s",a9);
+    cin>>num[9];
+
+
+    return 0;
+}
