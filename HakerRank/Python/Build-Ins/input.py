6999b150177b4b75ab04673e6f6972fe4455b3dc
sooaaib <soaib.safi@gmail.com>
2020-10-26 23:13:56 +0100

    :bookmark: Daily Update

---
+++ HakerRank/Python/Build-Ins/input.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 26 23:07:22 2020
+
+@author: Soaib
+Problem: Input()
+Link: https://www.hackerrank.com/challenges/input/problem
+"""
+x, k = map(int, input().split())
+print(k == eval(input()))
